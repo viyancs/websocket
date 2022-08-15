6e6b30d62dea47d6d68d155506773a3c7ea87e9a
viyancs <msofyancs@gmail.com>
2022-08-16 02:23:46 +0700

    adding C# client receive websocket

---
+++ C#/Client/Client/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Websocket.Client.Sample
+{
+    class Program
+    {
+        private static readonly ManualResetEvent ExitEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            InitLogging();
+
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
+            AssemblyLoadContext.Default.Unloading += DefaultOnUnloading;
+            Console.CancelKeyPress += ConsoleOnCancelKeyPress;
+
+            Console.WriteLine("|=======================|");
+            Console.WriteLine("|    WEBSOCKET CLIENT   |");
+            Console.WriteLine("|=======================|");
+            Console.WriteLine();
+
+            Log.Debug("====================================");
+            Log.Debug("              STARTING              ");
+            Log.Debug("====================================");
+
+            var factory = new Func<ClientWebSocket>(() =>
+            {
+                var client = new ClientWebSocket
+                {
+                    Options =
+                    {
+                        KeepAliveInterval = TimeSpan.FromSeconds(5),
+                        // Proxy = ...
+                        // ClientCertificates = ...
+                    }
+                };
+                client.Options.SetRequestHeader("Origin", "*");
+                return client;
+            });
+
+            var url = new Uri("ws://34.133.211.97:80/matchmake/");
+
+            using (IWebsocketClient client = new WebsocketClient(url, factory))
+            {
+                client.Name = "WHatever";
+                client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+                client.ErrorReconnectTimeout = TimeSpan.FromSeconds(30);
+                client.IsTextMessageConversionEnabled = true;
+                
+                client.ReconnectionHappened.Subscribe(info =>
+                {
+                    Log.Information($"Reconnection happened, type: {info.Type}, url: {client.Url}");
+                });
+                client.DisconnectionHappened.Subscribe(info =>
+                    Log.Warning($"Disconnection happened, type: {info.Type}"));
+
+                client.MessageReceived.Subscribe(msg =>
+                {
+                    Log.Information($"Message received: {msg}");
+                });
+
+                Log.Information("Starting...");
+                client.Start().Wait();
+                Log.Information("Started.");
+
+                Task.Run(() => StartSendingPing(client));
+                Task.Run(() => SwitchUrl(client));
+
+                ExitEvent.WaitOne();
+            }
+
+            Log.Debug("====================================");
+            Log.Debug("              STOPPING              ");
+            Log.Debug("====================================");
+            Log.CloseAndFlush();
+        }
+
+        private static async Task StartSendingPing(IWebsocketClient client)
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+
+                if (!client.IsRunning)
+                    continue;
+
+                client.Send("ping");
+            }
+        }
+
+        private static async Task SwitchUrl(IWebsocketClient client)
+        {
+            while (true)
+            {
+                await Task.Delay(20000);
+
+                var production = new Uri("ws://34.133.211.97:80/matchmake/");
+                var testnet = new Uri("ws://34.133.211.97:80/matchmake/");
+
+                var selected = client.Url == production ? testnet : production;
+                client.Url = selected;
+                await client.Reconnect();
+            }
+        }
+
+        private static void InitLogging()
+        {
+            var executingDir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+            var logPath = Path.Combine(executingDir, "logs", "verbose.log");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.File(logPath, rollingInterval: RollingInterval.Day)
+                .WriteTo.Console(LogEventLevel.Verbose,
+                    theme: AnsiConsoleTheme.Literate,
+                    outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message} {NewLine}{Exception}")
+                .CreateLogger();
+        }
+
+        private static void CurrentDomainOnProcessExit(object sender, EventArgs eventArgs)
+        {
+            Log.Warning("Exiting process");
+            ExitEvent.Set();
+        }
+
+        private static void DefaultOnUnloading(AssemblyLoadContext assemblyLoadContext)
+        {
+            Log.Warning("Unloading process");
+            ExitEvent.Set();
+        }
+
+        private static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Log.Warning("Canceling process");
+            e.Cancel = true;
+            ExitEvent.Set();
+        }
+    }
+}
